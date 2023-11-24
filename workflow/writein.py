b34728c1038b8a8d50fb555b6c678f21dc2e76ae
kihara <limuyuan3650@foxmail.com>
2023-11-24 22:21:36 +0800

    writein

--- workflow/writein.py
+++ workflow/writein.py
@@ -1,6 +1,3 @@
-from info_downloader.info_downloader import VideoInfoDownloader
-from subtitle_downloader.subtitle_downloader import SubtitleDownloader
-
 def writein(video_info, subtitle):
     with open(f"{video_info['info']['title']}.md", "w") as f:
         f.write(f"# {video_info['info']['title']}\n")
